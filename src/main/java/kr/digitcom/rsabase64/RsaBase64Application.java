16bc89ba9d08fd8143e97deb6138655e6b00438c
younghoon park <digitcom@hdmedi.co.kr>
2021-09-29 08:42:02 +0900

    Initial commit

---
+++ src/main/java/kr/digitcom/rsabase64/RsaBase64Application.java
@@ -0,0 +1,13 @@
+package kr.digitcom.rsabase64;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RsaBase64Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RsaBase64Application.class, args);
+    }
+
+}
---
+++ src/test/java/kr/digitcom/rsabase64/RsaBase64ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.digitcom.rsabase64;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RsaBase64ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
